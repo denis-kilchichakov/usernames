3c8faca909ad8ca4e9375498af7e47088a54425c
Denis Kilchichakov <kdenis87@gmail.com>
2023-07-01 13:00:39 +0400

    added common stackexchange request code

---
+++ services/stackexchange/stackexchange.go
@@ -0,0 +1,47 @@
+package stackexchange
+
+import (
+	"encoding/json"
+	"net/url"
+
+	"github.com/denis-kilchichakov/usernames/network"
+)
+
+type StackExchangeAPI interface {
+	Check(username string, site string, client network.RESTClient) (bool, error)
+}
+
+type StackExchange struct{}
+
+func (s StackExchange) Check(username string, site string, client network.RESTClient) (bool, error) {
+	apiURL := "https://api.stackexchange.com/2.3/users"
+	params := url.Values{}
+	params.Set("order", "desc")
+	params.Set("sort", "reputation")
+	params.Set("inname", username)
+	params.Set("site", site)
+	apiURL += "?" + params.Encode()
+
+	req := network.NewRequest("GET", apiURL, nil)
+	respBody, err := client.RetrieveBody(req)
+	if err != nil {
+		return false, err
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(respBody, &data)
+	if err != nil {
+		return false, err
+	}
+
+	users := data["items"].([]interface{})
+	for _, user := range users {
+		userData := user.(map[string]interface{})
+		displayName := userData["display_name"].(string)
+		if displayName == username {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
---
+++ services/stackexchange/stackexchange_test.go
@@ -0,0 +1,105 @@
+package stackexchange
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/denis-kilchichakov/usernames/network"
+	"github.com/stretchr/testify/assert"
+)
+
+// check error on request
+func TestStackExchangeErrorOnRequest(t *testing.T) {
+	expectedErr := fmt.Errorf("Some error")
+	mockClient := &network.TestRESTClient{
+		Body:  nil,
+		Error: expectedErr,
+	}
+
+	stackExchange := StackExchange{}
+
+	result, err := stackExchange.Check("someUser", "stackoverflow", mockClient)
+
+	assert.Equal(t, expectedErr, err)
+	assert.False(t, result)
+	request := mockClient.Requests[0]
+	expectedURL := "https://api.stackexchange.com/2.3/users?inname=someUser&order=desc&site=stackoverflow&sort=reputation"
+	expectedRequest := network.NewRequest("GET", expectedURL, nil)
+	assert.Len(t, mockClient.Requests, 1)
+	assert.Equal(t, expectedRequest, request)
+}
+
+func TestStackExchangeErrorOnJson(t *testing.T) {
+	mockClient := &network.TestRESTClient{
+		Body:  []byte(`malformed json`),
+		Error: nil,
+	}
+	stackExchange := StackExchange{}
+
+	result, err := stackExchange.Check("someUser", "stackoverflow", mockClient)
+
+	assert.Error(t, err)
+	assert.False(t, result)
+	request := mockClient.Requests[0]
+	expectedURL := "https://api.stackexchange.com/2.3/users?inname=someUser&order=desc&site=stackoverflow&sort=reputation"
+	expectedRequest := network.NewRequest("GET", expectedURL, nil)
+	assert.Len(t, mockClient.Requests, 1)
+	assert.Equal(t, expectedRequest, request)
+}
+
+func TestStackExchangeFound(t *testing.T) {
+	mockClient := &network.TestRESTClient{
+		Body: []byte(`{
+			"items": [
+				{
+					"display_name": "AwesomeUser"
+				},
+				{
+					"display_name": "someUser"
+				},
+				{
+					"display_name": "someUser3"
+				}
+			]
+		}`),
+		Error: nil,
+	}
+	stackExchange := StackExchange{}
+
+	result, err := stackExchange.Check("someUser", "stackoverflow", mockClient)
+
+	assert.NoError(t, err)
+	assert.True(t, result)
+	request := mockClient.Requests[0]
+	expectedURL := "https://api.stackexchange.com/2.3/users?inname=someUser&order=desc&site=stackoverflow&sort=reputation"
+	expectedRequest := network.NewRequest("GET", expectedURL, nil)
+	assert.Len(t, mockClient.Requests, 1)
+	assert.Equal(t, expectedRequest, request)
+}
+
+func TestStackExchangeNotFound(t *testing.T) {
+	mockClient := &network.TestRESTClient{
+		Body: []byte(`{
+			"items": [
+				{
+					"display_name": "AwesomeUser"
+				},
+				{
+					"display_name": "someUser3"
+				}
+			]
+		}`),
+		Error: nil,
+	}
+	stackExchange := StackExchange{}
+
+	result, err := stackExchange.Check("someUser", "stackoverflow", mockClient)
+
+	assert.NoError(t, err)
+	assert.False(t, result)
+	request := mockClient.Requests[0]
+	expectedURL := "https://api.stackexchange.com/2.3/users?inname=someUser&order=desc&site=stackoverflow&sort=reputation"
+	expectedRequest := network.NewRequest("GET", expectedURL, nil)
+	assert.Len(t, mockClient.Requests, 1)
+	assert.Equal(t, expectedRequest, request)
+}
